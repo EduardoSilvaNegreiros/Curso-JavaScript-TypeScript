136ae0aecf014839d10b3f170cc3ceb4430d1e92
eduardo <edunegreiross@gmail.com>
2024-09-04 12:00:00 -0300

    Explicação de código

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,9 +1,14 @@
+// Importa a biblioteca React, que é essencial para trabalhar com componentes em uma aplicação React.
 import React from 'react';
 
+// Importa o componente "Login" da pasta "pages". Este componente será exibido dentro do componente App.
 import Login from './pages/Login';
 
+// Define o componente funcional "App", que retorna o componente "Login".
+// Ou seja, quando o "App" for renderizado, o componente "Login" será exibido na tela.
 function App() {
   return <Login />;
 }
 
+// Exporta o componente "App" como padrão, permitindo que ele seja utilizado em outros arquivos.
 export default App;
