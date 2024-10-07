5c70ec10a2885daf843e9487fa11789b4d442a1b
eduardo <edunegreiross@gmail.com>
2024-10-07 09:24:02 -0300

    Atualização em main.js

--- REACT/lista_tarefas/src/components/Main.js
+++ REACT/lista_tarefas/src/components/Main.js
@@ -1,3 +1,7 @@
 import React, { Component } from 'react';
 
-export default class Main extends component {}
+export default class Main extends component {
+  render() {
+    return <h1>Meu componente com estado</h1>
+  }
+}
