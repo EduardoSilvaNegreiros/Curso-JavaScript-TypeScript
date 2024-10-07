b7687c90a5293309ada2c39f0cae855cd2d498c5
eduardo <edunegreiross@gmail.com>
2024-10-07 10:27:43 -0300

    Atualização das dependências do projeto

--- REACT/lista_tarefas/.eslintrc.js
+++ REACT/lista_tarefas/.eslintrc.js
@@ -1,31 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
-    node: true,
+    es6: true,
   },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'airbnb',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    requireConfigFile: false,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'react-hooks'],
+  plugins: [
+    'react',
+  ],
   rules: {
-    'prettier/prettier': 2,
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'no-undef': 'off',
+    "react/jsx-filename-extension": 0,
+    "react/state-in-constructor": 0,
+    "react/forbid-prop-types": 0
   },
 };
