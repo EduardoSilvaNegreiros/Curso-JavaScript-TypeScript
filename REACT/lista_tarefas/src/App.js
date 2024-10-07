4aaebbd22ade709365ac4908854d51ee18e95e95
eduardo <edunegreiross@gmail.com>
2024-10-07 11:49:36 -0300

    Resolução de bugs

--- REACT/lista_tarefas/src/App.js
+++ REACT/lista_tarefas/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; // Toda vez que for usar componentes REACT, se usa import react
+import React from 'react'; 
 import Main from './components/Main';
 
 import './App.css';
--- REACT/lista_tarefas/src/components/Main.js
+++ REACT/lista_tarefas/src/components/Main.js
@@ -4,20 +4,20 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-
-    };
-  }
+    this.state = {};
+  };
 
   render() {
     return (
       <div className="main">
-        <h1>Meu componente com estado</h1>
+        <h1>Meu componente com estado</h1>;
         <form action="#">
           <input type="text" />
           <button type="submit">Enviar</button>
         </form>
       </div>
     );
-  }
+  };
 }
+
+
