2db1c160eb4d473f2a1ac14934fcb1535049749e
eduardo <edunegreiross@gmail.com>
2024-10-20 21:55:05 -0300

    Explicação momentanea do código

--- REACT/lista_tarefas/src/components/Main.js
+++ REACT/lista_tarefas/src/components/Main.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // Importa React e Component para criar o componente de classe
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa'; // Importa ícone "plus" da biblioteca de ícones
 
-import './Main.css';
+import './Main.css'; // Importa o arquivo de estilos CSS
 
 export default class Main extends Component {
-  // Isso se chama class fields
+  // Define o estado inicial da classe (novaTarefa)
   state = {
-    novaTarefa: '',
+    novaTarefa: '', // Variável que armazena a nova tarefa
   };
 
+  // Função para atualizar o estado (novaTarefa) conforme o input é preenchido
   handleChange = (e) => {
     this.setState({
-      novaTarefa: e.target.value,
+      novaTarefa: e.target.value, // Atualiza o valor da tarefa com o input do usuário
     });
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa } = this.state; // Extrai novaTarefa do estado
 
     return (
       <div className="main">
-        <h1>Lista de tarefas</h1>
-        <form action="#" className="form">
-          <input onChange={this.handleChange} type="text" />
+        <h1>Lista de tarefas</h1> {/* Título da página */}
+        <form action="#" className="form"> {/* Formulário para adicionar tarefas */}
+          <input onChange={this.handleChange} type="text" /> {/* Input para digitar a tarefa */}
           <button type="submit">
-            <FaPlus />
+            <FaPlus /> {/* Botão com ícone de adição */}
           </button>
         </form>
       </div>
