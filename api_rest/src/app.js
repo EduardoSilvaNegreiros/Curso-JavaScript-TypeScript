fcd3cc0dd1cc9d2d2822023296758057c5f070ed
eduardo <edunegreiross@gmail.com>
2024-10-28 09:52:43 -0300

    Criando uma base para meus projetos em REACT

--- api_rest/src/app.js
+++ api_rest/src/app.js
@@ -2,46 +2,59 @@
 import './database';
 import { resolve } from 'path';
 
-// Importa o framework Express para criar o servidor e gerenciar rotas
+// Importa o framework Express e bibliotecas de segurança
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
-// Importa as rotas definidas em arquivos separados
+// Importa as rotas
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
 
+// Define a lista de origens permitidas para CORS
+const whitelist = [
+  'https://react1.eduardonegreiros.com.br',
+  'http://localhost:3000',
+];
+
+// Configurações de CORS
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// Classe principal da aplicação
 class App {
   constructor() {
-    // Cria uma instância da aplicação Express
     this.app = express();
-
-    // Configura middlewares e rotas
-    this.initializeMiddlewares();
-    this.initializeRoutes();
+    this.middlewares();
+    this.routes();
   }
 
-  // Método que configura os middlewares
-  initializeMiddlewares() {
+  middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
-
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
-  // Método que define as rotas da aplicação
-  initializeRoutes() {
+  routes() {
     this.app.use('/', homeRoutes);
-
     this.app.use('/users/', userRoutes);
-
     this.app.use('/tokens/', tokenRoutes);
-
     this.app.use('/alunos/', alunoRoutes);
     this.app.use('/fotos/', fotoRoutes);
   }
 }
 
-// Exporta a instância da aplicação Express para ser utilizada no servidor
+// Exporta a instância da aplicação
 export default new App().app;
