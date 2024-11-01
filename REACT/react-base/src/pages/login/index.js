d4e95bc7b331ee1912f7d0513d1847127f08d227
eduardo <edunegreiross@gmail.com>
2024-10-31 23:18:06 -0300

    Aumentando meu componente para teste

--- REACT/react-base/src/pages/login/index.js
+++ REACT/react-base/src/pages/login/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <h1>
+        Login
+        <small>Oie</small>
+      </h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+    
+  );
 }
