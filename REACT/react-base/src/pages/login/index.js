240e22d4c75d4162144cea0a1b48cf10d10f6749
eduardo <edunegreiross@gmail.com>
2024-11-01 12:00:00 -0300

    Descrição do commit

--- REACT/react-base/src/pages/login/index.js
+++ REACT/react-base/src/pages/login/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'; // Importa a biblioteca React, necessária para criar componentes React.
 
-import { Tittle } from './styled';
+import { Tittle } from './styled'; // Importa o componente Tittle (título) de um arquivo de estilos.
 
 export default function Login() {
+  // Define e exporta a função Login como um componente padrão.
   return (
+    // Início do retorno do componente, que deve retornar JSX.
     <>
+      {/* Fragmento React, utilizado para agrupar múltiplos elementos sem adicionar nós extras ao DOM. */}
       <Tittle isRed={false}>
-        Login
+        {' '}
         <small>Oie</small>
       </Tittle>
       <p>Lorem ipsum dolor sit amet.</p>
     </>
-  );
+  ); // Fim do retorno do componente.
 }
