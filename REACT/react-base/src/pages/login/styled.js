434d24755cf5fbb345485a8cf947acac8812979b
eduardo <edunegreiross@gmail.com>
2024-11-03 12:00:00 -0300

    Explicação de código

--- REACT/react-base/src/pages/login/styled.js
+++ REACT/react-base/src/pages/login/styled.js
@@ -1,5 +1,10 @@
+// Importa a função 'styled' da biblioteca 'styled-components', que permite criar componentes de estilo.
 import styled from 'styled-components';
 
+// Cria e exporta um componente chamado 'Tittle' que é uma tag HTML <h1> estilizada.
+// eslint-disable-next-line import/prefer-default-export
 export const Tittle = styled.h1`
+  // Define a cor do texto, com base em uma propriedade 'isRed' passada para o componente.
+  // Se 'props.isRed' for verdadeira, a cor será 'red' (vermelha), caso contrário, será 'blue' (azul).
   color: ${props => (props.isRed ? 'red' : 'blue')};
 `;
