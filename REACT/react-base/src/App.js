f1d8ec2ce79c45dcc4b171f0287717c933a887fd
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-07 11:33:20 -0300

    Importando meu componente de estilização global

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,14 +1,16 @@
 // Importa a biblioteca React, que é essencial para trabalhar com componentes em uma aplicação React.
 import React from 'react';
 
-// Importa o componente "Login" da pasta "pages". Este componente será exibido dentro do componente App.
 import Login from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
 
-// Define o componente funcional "App", que retorna o componente "Login".
-// Ou seja, quando o "App" for renderizado, o componente "Login" será exibido na tela.
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <GlobalStyles />
+    </>
+  );
 }
 
-// Exporta o componente "App" como padrão, permitindo que ele seja utilizado em outros arquivos.
 export default App;
