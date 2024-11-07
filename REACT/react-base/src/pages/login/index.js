351b68ae4b2b60bddbdacf4ce313f7c572d8b640
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-07 10:49:48 -0300

    Organização do projeto, tentativa de resolução de bug

--- REACT/react-base/src/pages/login/index.js
+++ REACT/react-base/src/pages/login/index.js
@@ -1,18 +1,13 @@
-import React from 'react'; // Importa a biblioteca React, necessária para criar componentes React.
-
-import { Tittle } from './styled'; // Importa o componente Tittle (título) de um arquivo de estilos.
+import React from 'react';
+import { Tittle } from './styled';
 
 export default function Login() {
-  // Define e exporta a função Login como um componente padrão.
   return (
-    // Início do retorno do componente, que deve retornar JSX.
     <>
-      {/* Fragmento React, utilizado para agrupar múltiplos elementos sem adicionar nós extras ao DOM. */}
       <Tittle isRed={false}>
-        {' '}
         <small>Oie</small>
       </Tittle>
       <p>Lorem ipsum dolor sit amet.</p>
     </>
-  ); // Fim do retorno do componente.
+  );
 }
--- REACT/react-base/src/pages/login/styled.js
+++ REACT/react-base/src/pages/login/styled.js
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 // Cria e exporta um componente chamado 'Tittle' que é uma tag HTML <h1> estilizada.
 // eslint-disable-next-line import/prefer-default-export
 export const Tittle = styled.h1`
-  // Define a cor do texto, com base em uma propriedade 'isRed' passada para o componente.
-  // Se 'props.isRed' for verdadeira, a cor será 'red' (vermelha), caso contrário, será 'blue' (azul).
-  color: ${props => (props.isRed ? 'red' : 'blue')};
+  small {
+    font-size: 12pt;
+    margin-left: 15px;
+    color: #999;
+  }
 `;
