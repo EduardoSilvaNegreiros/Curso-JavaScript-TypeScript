6e367462ef370900bf8f31da92e64d6c24f08a8b
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-09 13:46:12 -0300

    Estilizando meu container

--- REACT/react-base/src/pages/login/index.js
+++ REACT/react-base/src/pages/login/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import { Container } from '../../styles/GlobalStyles';
+
 import { Tittle, Paragrafo } from './styled';
 
 export default function Login() {
   return (
-    <>
+    <Container>
       <Tittle>
         <small>Oie</small>
       </Tittle>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
       <a href="#">Oie</a>
-    </>
+    </Container>
   );
 }
