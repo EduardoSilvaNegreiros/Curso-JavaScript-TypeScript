4b8651615e41b15302a2c00525f2477f229cf185
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-18 12:33:15 -0300

    Fazendo testes com reducer

--- REACT/react-base/src/store/index.js
+++ REACT/react-base/src/store/index.js
@@ -3,6 +3,7 @@ import { legacy_createStore as createStore } from 'redux';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'BOTAO_CLICADO':
+      console.log('Estou ouvindo BOTAO_CLICADO');
       return state;
 
     default:
