ddba1e514e226d60815fae4d598c09990350d816
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-22 22:55:45 -0300

    Criando consumir-api

---
+++ REACT/consumirapi/src/pages/Login/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Tittle, Paragrafo } from './styled';
+import * as exampleAction from '../../store/modules/example/actions';
+
+export default function Login() {
+  // Obtém a referência para a função dispatch do Redux
+  const dispatch = useDispatch();
+  function handleClick(e) {
+    e.preventDefault();
+
+    // Dispara uma ação para o Redux (mas não está enviando nenhuma ação atualmente)
+    dispatch(exampleAction.clicaBotaoRequest());
+  }
+
+  // Retorna o JSX do componente
+  return (
+    <Container>
+      <Tittle>
+        <small>Oie</small>
+      </Tittle>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
+    </Container>
+  );
+}
