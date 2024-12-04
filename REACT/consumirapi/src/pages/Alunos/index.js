c5082b11f06185b6fc3adcc9e1e5be594addfd8d
Eduardo <edunegreiross@gmail.com>
2024-12-04 12:00:00 -0300

    Reformulando o código

--- REACT/consumirapi/src/pages/Alunos/index.js
+++ REACT/consumirapi/src/pages/Alunos/index.js
@@ -25,43 +25,11 @@ export default function Alunos() {
     getData();
   }, []);
 
-  const handleDeleteAsk = (e) => {
-    e.preventDefault();
-    const exclamation = e.currentTarget.nextSibling;
-    exclamation.setAttribute('display', 'block');
-    e.currentTarget.remove();
-  };
-
-  const handleDelete = async (e, id, index) => {
-    e.persist();
-
-    try {
-      setIsLoading(true);
-      await axios.delete(`/alunos/${id}`);
-      const novosAlunos = [...alunos];
-      novosAlunos.splice(index, 1);
-      setAlunos(novosAlunos);
-      setIsLoading(false);
-    } catch (err) {
-      const status = get(err, 'response.status', 0);
-
-      if (status === 401) {
-        toast.error('Você precisa fazer login');
-      } else {
-        toast.error('Ocorreu um erro ao excluir aluno');
-      }
-
-      setIsLoading(false);
-    }
-  };
 
   return (
     <Container>
-      <Loading isLoading={isLoading} />
 
       <h1>Alunos</h1>
-
-      <NovoAluno to="/aluno/">Novo aluno</NovoAluno>
 
       <AlunoContainer>
         {alunos.map((aluno, index) => (
