43ebea9ad9486c3c0e132deaddbcbd524d8564bb
Eduardo <edunegreiross@gmail.com>
2024-12-05 12:00:00 -0300

    Utilizando formulário

--- REACT/consumirapi/src/pages/Register/index.js
+++ REACT/consumirapi/src/pages/Register/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Register() {
   return (
     <Container>
       <h1>Crie sua conta</h1>
+
+      <Form>
+        <label htmlFor=""></label>
+      </Form>
     </Container>
   );
 }
