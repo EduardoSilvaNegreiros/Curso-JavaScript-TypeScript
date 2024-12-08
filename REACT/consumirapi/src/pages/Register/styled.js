007288ad5aa495f70a26195f2d65101456e823f1
Eduardo <edunegreiross@gmail.com>
2024-12-08 12:00:00 -0300

    Estilizando os inputs

--- REACT/consumirapi/src/pages/Register/styled.js
+++ REACT/consumirapi/src/pages/Register/styled.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+  margin-top: 20px;
 
   label {
     display: flex;
     flex-direction: column;
+    margin-bottom: 20px;
   }
 `;
 
