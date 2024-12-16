98bf9c1268e17368e18beef6a7cec58f24282f5c
Eduardo <edunegreiross@gmail.com>
2024-12-16 12:00:00 -0300

    usando state para senha

--- REACT/consumirapi/src/pages/Login/index.js
+++ REACT/consumirapi/src/pages/Login/index.js
@@ -5,17 +5,25 @@ import { Form } from './styled';
 
 export default function Login() {
   const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   return (
     <Container>
-      <h1>Login {email}</h1>
+      <h1>Login</h1>
 
       <Form>
         <input
           type="text"
           value={email}
           onChange={e => setEmail(e.target.value)}
-          placeholder='Seu e-mail'
+          placeholder="Seu e-mail"
+        />
+
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="Sua senha"
         />
       </Form>
     </Container>
