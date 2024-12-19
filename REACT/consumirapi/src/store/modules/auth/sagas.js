e4999309c4cf754d674f66f576fb76387ab67e77
Eduardo <edunegreiross@gmail.com>
2024-12-19 12:00:00 -0300

    limpando o sagas.js de auth

--- REACT/consumirapi/src/store/modules/auth/sagas.js
+++ REACT/consumirapi/src/store/modules/auth/sagas.js
@@ -4,21 +4,6 @@ import { toast } from 'react-toastify';
 import * as actions from './actions';
 import * as types from '../types';
 
-const requisicao = () =>
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, 600);
-  });
-
-function* exampleRequest() {
-  try {
-    yield call(requisicao);
-    yield put(actions.clicaBotaoSuccess());
-  } catch (error) {
-    toast.error('Deu erro. ');
-    yield put(actions.clicaBotaoFailure());
-  }
-}
+function* exampleRequest() {}
 
 export default all([takeLatest(types.BOTAO_CLICADO_REQUEST, exampleRequest)]);
