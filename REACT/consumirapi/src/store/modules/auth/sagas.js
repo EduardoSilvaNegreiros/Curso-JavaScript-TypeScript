d68585dc1573e1425efd0f371a667123ea767cb7
Eduardo <edunegreiross@gmail.com>
2024-12-21 12:00:00 -0300

    capturando os erros dentro do sagas.js

--- REACT/consumirapi/src/store/modules/auth/sagas.js
+++ REACT/consumirapi/src/store/modules/auth/sagas.js
@@ -3,9 +3,14 @@ import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import * as actions from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
 
 function* loginRequest({ payload }) {
-  console.log('SAGA', payload);
+  try {
+    
+  } catch (error) {
+    toast.error('Usuário ou senha inválidos');
+  }
 }
 
 export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
