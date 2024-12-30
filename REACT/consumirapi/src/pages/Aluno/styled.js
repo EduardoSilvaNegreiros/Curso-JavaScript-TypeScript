86b23f26d31f4917926b8b09ffe0e73647ab7cc2
Eduardo <edunegreiross@gmail.com>
2024-12-30 00:00:00 -0300

    Terminando a parte de cadastro do aluno

--- REACT/consumirapi/src/pages/Aluno/styled.js
+++ REACT/consumirapi/src/pages/Aluno/styled.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
-export const AlunoContainer = styled.div`
+export const Form = styled.form`
   margin-top: 20px;
-`;
+  display: flex;
+  flex-direction: column;
 
-export const ProfilePicture = styled.div`
-  img {
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
+  input {
+    height: 40px;
+    margin-bottom: 20px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 0 10px;
   }
 `;
